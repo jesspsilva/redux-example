4da369e551e4b3bc699da9ef8deaba7f1a52308e
jesspsilva <jps936@hotmail.com>
2021-01-31 18:51:34 +0000

    Clean reducers with utility functions

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const initialState = {
     results: []
@@ -7,10 +8,10 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {...state, results: state.results.concat({id: new Date(), value: action.counter})}
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.counter})})
         case actionTypes.DELETE_RESULT:
             const updatedArray = state.results.filter(result => result.id !== action.elId)
-            return {...state, results: updatedArray}
+            return updateObject(state, {results: updatedArray})
     }
     return state;
 };
