c46dc94c8088a8bc8e0c132a56b741b5959afbd1
jesspsilva <jps936@hotmail.com>
2021-01-31 18:33:07 +0000

    Handle asynchronous with redux thunk

---
+++ src/store/actions/result.js
@@ -0,0 +1,21 @@
+import * as actionTypes from './actionTypes';
+
+export const saveResult = (ctr) => {
+    return  {
+        type: actionTypes.STORE_RESULT,
+        counter: ctr
+    }
+}
+export const storeResult = (ctr) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(saveResult(ctr))
+        }, 2000);
+    }
+};
+export const deleteResult = (id) => {
+    return {
+        type: actionTypes.DELETE_RESULT,
+        elId: id
+    }
+};
